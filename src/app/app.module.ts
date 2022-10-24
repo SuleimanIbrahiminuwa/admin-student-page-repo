599c7d29a6dcde0591ce9b30310070320be51597
deca <isuleimanibrahim@gmail.com>
2022-10-24 09:37:22 +0100

    add changes to the search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import { Ng2FilterPipeModule } from 'ng2-filter-pipe'
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { TableComponentComponent } from './component/table-component/table-component.component';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @NgModule({
   declarations: [
     AppComponent,
     TableComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2FilterPipeModule,
+    FormsModule,
+    HttpClientModule,
+    Ng2SearchPipeModule
     
   ],
   providers: [],
--- src/app/component/table-component/table-component.component.ts
+++ src/app/component/table-component/table-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-table-component',
@@ -7,9 +8,177 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponentComponent implements OnInit {
 
+  searchTerm :any
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  public userDetails =[
+       
+    {
+        StudentName:'Suleiman Ibrahim Inuwa',
+        PhoneNumber:'08144832703',
+        University:'Abubakar Tafawa Balewa Uni.',
+        Course:'Computer and Comm. Engineering',
+        LoanAmountRequested:'₦150,000',
+        LoanAmountDisburse:'₦100,000',
+        SponsorName:'Amb Musa Saban',
+        ProfileVerifyStatus:"Yes"
+    },
+    {
+      StudentName:'Suleiman Ibrahim Inuwa',
+      PhoneNumber:'08144832703',
+      University:'Abubakar Tafawa Balewa Uni.',
+      Course:'Computer and Comm. Engineering',
+      LoanAmountRequested:'₦150,000',
+      LoanAmountDisburse:'₦100,000',
+      SponsorName:'Amb Musa Saban',
+      ProfileVerifyStatus:"Yes"
+  },
+  {
+    StudentName:'Suleiman Ibrahim Inuwa',
+    PhoneNumber:'08144832703',
+    University:'University of Maiduguri.',
+    Course:'Computer and Comm. Engineering',
+    LoanAmountRequested:'₦150,000',
+    LoanAmountDisburse:'₦100,000',
+    SponsorName:'Amb Musa Saban',
+    ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer Science',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Umar Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+},
+{
+  StudentName:'Suleiman Ibrahim Inuwa',
+  PhoneNumber:'08144832703',
+  University:'Abubakar Tafawa Balewa Uni.',
+  Course:'Computer and Comm. Engineering',
+  LoanAmountRequested:'₦150,000',
+  LoanAmountDisburse:'₦100,000',
+  SponsorName:'Amb Musa Saban',
+  ProfileVerifyStatus:"Yes"
+}
+];
+
 
 }
